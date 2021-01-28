96656830a75b489df636574cf128392d6cd8573c
ymauri <ymauri@outlook.com>
2021-01-28 23:14:00 +0100

    Fix post number

--- wp-content/plugins/thepenthouse/scripts/scripts_hotelchamp.php
+++ wp-content/plugins/thepenthouse/scripts/scripts_hotelchamp.php
@@ -2,12 +2,12 @@
 
 function all_pages_script() {
     //Booking done
-    if (is_page ('80') || is_page ('78') || is_page ('77')) {
+    if (is_page ('80') || is_page ('78')) {
         ?>  
             <script>window.hcFinished=true;</script>
         <?php
     //Booking engine pages
-    } else if (is_page ('76') || is_page ('75') || is_page ('84')) {
+    } else if (is_page ('76') || is_page ('75') || is_page ('84') || is_page('77')) {
         ?>  
             <script>window.hcBooking=true;</script>
         <?php
