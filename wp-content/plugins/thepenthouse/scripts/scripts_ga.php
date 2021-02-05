2591c2c5aa5a7c2f6adab8262644ca223a25981c
ymauri <ymauri@outlook.com>
2021-02-05 15:24:11 +0100

    Fix ga ID

--- wp-content/plugins/thepenthouse/scripts/scripts_ga.php
+++ wp-content/plugins/thepenthouse/scripts/scripts_ga.php
@@ -2,15 +2,8 @@
 
 function ga_script() {
     ?>  
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S1CLFKG6YC"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-S1CLFKG6YC');
-        </script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-261093413-1"></script>
+        <script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-261093413-1');</script>
     <?php
 }
 add_action('wp_head', 'ga_script');
