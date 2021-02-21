9597935fee69348b38cf4311fbf8844d45f6baad
ymauri <ymauri@outlook.com>
2021-02-21 23:53:08 +0100

    Update cron function name

--- wp-content/plugins/thepenthouse/scripts/delete_old_blocked_deactivate.php
+++ wp-content/plugins/thepenthouse/scripts/delete_old_blocked_deactivate.php
@@ -1,18 +1,18 @@
 <?php
 function delete_old_blocked_deactivate() {
-    wp_clear_scheduled_hook( 'delete_old_blocked_cron' );
+    wp_clear_scheduled_hook( 'delete_old_blocked' );
 }
  
 add_action('init', function() {
-    add_action( 'delete_old_blocked_cron', 'delete_old_blocked_run_cron' );
+    add_action( 'delete_old_blocked', 'delete_old_blocked_run' );
     register_deactivation_hook( __FILE__, 'delete_old_blocked_deactivate' );
  
-    if (! wp_next_scheduled ( 'delete_old_blocked_cron' )) {
-        wp_schedule_event( time(), 'weekly', 'delete_old_blocked_cron' );
+    if (! wp_next_scheduled ( 'delete_old_blocked' )) {
+        wp_schedule_event( strtotime('11:02:00'), 'daily', 'delete_old_blocked' );
     }
 });
  
-function delete_old_blocked_run_cron() {
+function delete_old_blocked_run() {
     $calendar  = new Calendar();
     $calendar->deletePast();
 }
