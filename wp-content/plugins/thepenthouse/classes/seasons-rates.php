8a72542f2ff2feded0a35683c145ec9dce2e690d
ymauri <ymauri@outlook.com>
2021-03-09 23:18:12 +0100

    Discribe functionlities

--- wp-content/plugins/thepenthouse/classes/seasons-rates.php
+++ wp-content/plugins/thepenthouse/classes/seasons-rates.php
@@ -7,7 +7,14 @@ class SeasonsRates
         $this->guesty = new Guesty();
     }
 
-    public function cresteSeason(string $date) {
+    /**
+     * Generate season post
+     * @param string $date
+     * 
+     * @return int
+     */
+    public function cresteSeason(string $date)
+    {
         $seasonId = $this->searchSeason($date, $date);
         if (empty($seasonId)) {
             $seasonId = wp_insert_post([
@@ -15,18 +22,27 @@ class SeasonsRates
                 "post_status" => 'publish',
                 "comment_status" => "closed",
                 "ping_status" => "closed",
-                "post_name" => 'season'.$date.'to'.$date,
-                "post_title" => 'season'.$date.'to'.$date
+                "post_name" => 'season' . $date . 'to' . $date,
+                "post_title" => 'season' . $date . 'to' . $date
             ], false, false);
 
             add_post_meta($seasonId, 'mphb_start_date', $date);
             add_post_meta($seasonId, 'mphb_end_date', $date);
-            add_post_meta($seasonId, 'mphb_days', [0,1,2,3,4,5,6]);
+            add_post_meta($seasonId, 'mphb_days', [0, 1, 2, 3, 4, 5, 6]);
         }
         return $seasonId;
     }
 
-    private function createRates(int $roomType, string $seasonId, int $price ) {        
+    /**
+     * Generate rates
+     * @param int $roomType
+     * @param string $seasonId
+     * @param int $price
+     * 
+     * @return void
+     */
+    private function createRates(int $roomType, string $seasonId, int $price)
+    {
         $rateId = $this->searchRate($roomType);
         if (empty($rateId)) {
             $rateId = wp_insert_post([
@@ -34,20 +50,20 @@ class SeasonsRates
                 "post_status" => 'publish',
                 "comment_status" => "closed",
                 "ping_status" => "closed",
-                "post_name" => 'roomType-'.$roomType,
-                "post_title" => 'roomType-'.$roomType
+                "post_name" => 'roomType-' . $roomType,
+                "post_title" => 'roomType-' . $roomType
             ], false, false);
-    
+
             add_post_meta($rateId, 'mphb_room_type_id', $roomType);
         }
 
-        $rateSeasons = get_post_meta( $rateId, 'mphb_season_prices', true );
+        $rateSeasons = get_post_meta($rateId, 'mphb_season_prices', true);
 
         if (!empty($rateSeasons)) {
             $key = array_search($seasonId, array_column($rateSeasons, 'season'));
             if ($key !== false) {
-                $rateSeasons[$key]['price']['prices'] = [$price]; 
-                update_post_meta($rateId, 'mphb_season_prices', $rateSeasons);        
+                $rateSeasons[$key]['price']['prices'] = [$price];
+                update_post_meta($rateId, 'mphb_season_prices', $rateSeasons);
                 return;
             }
         }
@@ -61,13 +77,21 @@ class SeasonsRates
                 'prices'    => [$price],
                 'enable_variations' => false,
                 'variations' => []
-            ]]; 
-            update_post_meta($rateId, 'mphb_season_prices', $rateSeasons);        
+            ]
+        ];
+        update_post_meta($rateId, 'mphb_season_prices', $rateSeasons);
         return;
-    
     }
 
-    private function searchSeason (string $startDate, string $endDate) {
+    /**
+     * Search season by dates
+     * @param string $startDate
+     * @param string $endDate
+     * 
+     * @return int
+     */
+    private function searchSeason(string $startDate, string $endDate)
+    {
         $seasons = new WP_Query([
             'post_type' => 'mphb_season',
             'post_status' => 'publish',
@@ -96,11 +120,18 @@ class SeasonsRates
         return $seasonId;
     }
 
-    private function searchRate (int $roomType) {
+    /**
+     * Search rates by post_title (accommodation_type_id)
+     * @param int $roomType
+     * 
+     * @return [type]
+     */
+    private function searchRate(int $roomType)
+    {
         $rates = get_posts([
             'post_type'      => 'mphb_rate',
-            'title'          => 'roomType-'.$roomType,
-            'posts_per_page' => -1 
+            'title'          => 'roomType-' . $roomType,
+            'posts_per_page' => -1
         ]);
         foreach ($rates as $rate) {
             return $rate->ID;
@@ -108,40 +139,55 @@ class SeasonsRates
         return null;
     }
 
-    public function syncSeasonsView() {
+    /**
+     * Render view for generating seasons
+     * @return void
+     */
+    public function syncSeasonsView()
+    {
         $roomTypes = new WP_Query([
             'post_type' => 'mphb_room_type',
-            'posts_per_page'=>-1
+            'posts_per_page' => -1
         ]);
-        ?>
+?>
         <form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
             <h4>Select the accommodation types</h4>
             <ul>
-            <?php while ($roomTypes->have_posts()) { 
-                $roomTypes->the_post();?>
-                <li style="list-style: none;"><input type="checkbox" name="roomTypes[]" checked value="<?php echo get_the_ID();?>"><?php echo get_the_title();?></li>
-            <?php } ?>
+                <?php while ($roomTypes->have_posts()) {
+                    $roomTypes->the_post(); ?>
+                    <li style="list-style: none;"><input type="checkbox" name="roomTypes[]" checked value="<?php echo get_the_ID(); ?>"><?php echo get_the_title(); ?></li>
+                <?php } ?>
             </ul>
             <label>Select the initial date</label>
             <input class="datepicker" name="startDate" id="startDate">
 
             <label>Select the end date</label>
             <input class="datepicker" name="endDate" id="endDate">
-            <br/><br/>
-            
+            <br /><br />
+
             <label>Price €</label>
             <input name="price" id="price">
-            <br/><br/>
+            <br /><br />
 
             <input type='hidden' name="action" value='tph_seasons_rates_create' class='button'>
             <input type='submit' name="update" value='Update seasons' class='button'>
         </form>
-        <?php
+<?php
     }
 
-    public function syncSeasons(array $roomTypes, int $price, string $startDate, string $endDate) {
-        $loopStartDate = $startDate;     
-        $seasonsIds = [];   
+    /**
+     * Allows to manage seasons and rates relationships
+     * @param array $roomTypes
+     * @param int $price
+     * @param string $startDate
+     * @param string $endDate
+     * 
+     * @return void
+     */
+    public function syncSeasons(array $roomTypes, int $price, string $startDate, string $endDate)
+    {
+        $loopStartDate = $startDate;
+        $seasonsIds = [];
         while (new DateTime($loopStartDate) <= new DateTime($endDate)) {
             $seasonsIds[] = $this->cresteSeason($loopStartDate);
             $loopStartDate = date('Y-m-d', strtotime($loopStartDate . ' +1 day'));
@@ -154,7 +200,14 @@ class SeasonsRates
         }
     }
 
-    public function updatePrice(array $dataCalendar) {
+    /**
+     * Update prices that came from guesty platform
+     * @param array $dataCalendar
+     * 
+     * @return void
+     */
+    public function updatePrice(array $dataCalendar)
+    {
         $first = array_key_first($dataCalendar);
         $listingId = $dataCalendar[$first]['listingId'];
 
@@ -167,29 +220,33 @@ class SeasonsRates
 
         $roomTypes = [];
         foreach ($rooms as $room) {
-            $roomType =  get_post_meta( $room->ID, 'mphb_room_type_id', true );
-            if (!empty(get_post_meta( $roomType, 'update_price', true ))) {
+            $roomType =  get_post_meta($room->ID, 'mphb_room_type_id', true);
+            if (!empty(get_post_meta($roomType, 'update_price', true))) {
                 $roomTypes[] = $roomType;
-            }            
+            }
         }
 
-        foreach ($dataCalendar as $calendar) { 
+        foreach ($dataCalendar as $calendar) {
             if ($calendar['status'] != "booked") {
-                $this->syncSeasons($roomTypes, $calendar['price'], $calendar['date'], $calendar['date']);    
+                $this->syncSeasons($roomTypes, $calendar['price'], $calendar['date'], $calendar['date']);
             }
         }
     }
 
-    public function deleteOldSeasons() {
-        $date = date("Y-m-d",strtotime(date("Y-m-d")." - 1 days"));
+    /**
+     * Job task. Delete past mphb_season post_type
+     * @return void
+     */
+    public function deleteOldSeasons()
+    {
+        $date = date("Y-m-d", strtotime(date("Y-m-d") . " - 1 days"));
         $seasons = get_posts([
-            'post_type' => 'mphb_room',
-            "post_name" => 'season'.$date.'to'.$date,
+            'post_type' => 'mphb_season',
+            "post_name" => 'season' . $date . 'to' . $date,
         ]);
 
         foreach ($seasons as $season) {
             wp_delete_post($season->ID, true);
         }
     }
-
 }
