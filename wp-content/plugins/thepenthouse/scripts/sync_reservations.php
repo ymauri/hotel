d569390086ae0175dcd2a4ab52ad362ba481dbf5
ymauri <ymauri@outlook.com>
2021-06-14 21:28:44 +0200

    Add mphb_payment_completed hook for update guesty payment details.

--- wp-content/plugins/thepenthouse/scripts/sync_reservations.php
+++ wp-content/plugins/thepenthouse/scripts/sync_reservations.php
@@ -2,6 +2,7 @@
 
 add_action('mphb_booking_confirmed', 'update_reservation');
 add_action('mphb_booking_cancelled', 'update_reservation');
+add_action('mphb_payment_completed', 'update_payment_detail', 8);
 
 function update_reservation($booking)
 {
@@ -9,6 +10,21 @@ function update_reservation($booking)
         $reservation = new Reservation();
         $reservation->setBooking($booking);
         $reservation->put();
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
+
+
+function update_payment_detail($payment)
+{
+    $postId = $payment->getId();
+    try {
+            $bookingID = get_post_meta($postId, '_mphb_booking_id', true);            
+            $reservation = new Reservation();
+            $booking = MPHB()->getBookingRepository()->findById($bookingID);  
+            $reservation->setBooking($booking);
+            $reservation->put();
     } catch (Exception $e) {
         throw $e;
     }
