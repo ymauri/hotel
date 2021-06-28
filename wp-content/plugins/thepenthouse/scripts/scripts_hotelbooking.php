8a457a32ed6a36f0082464c09218c81092982cae
ymauri <ymauri@outlook.com>
2021-06-28 21:34:41 +0200

    Include prefix on phone field

--- wp-content/plugins/thepenthouse/scripts/scripts_hotelbooking.php
+++ wp-content/plugins/thepenthouse/scripts/scripts_hotelbooking.php
@@ -30,3 +30,63 @@ function guest_select_behaviour() {
 }
 add_action('wp_footer', 'guest_select_behaviour');
 
+function phone_field() {
+   
+    ?>  
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/css/intlTelInput.css" integrity="sha512-gxWow8Mo6q6pLa1XH/CcH8JyiSDEtiwJV78E+D+QP0EVasFs8wKXq16G8CLD4CJ2SnonHr4Lm/yY2fSI2+cbmw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+       <!-- Phone field -->
+       <style type="text/css">
+            .iti__flag {
+                background-image: url("https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/img/flags.png");
+            }
+
+            #mphb_phone {
+                padding-left: 57px !important;
+            }
+
+            .iti.iti--allow-dropdown {
+                width: 100% !important;
+            }
+            @media (-webkit-min-device-pixel-ratio: 2),
+            (min-resolution: 192dpi) {
+                .iti__flag {
+                    background-image: url("https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/img/flags@2x.png");
+                }
+            }
+       </style>
+       
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/js/intlTelInput.min.js" integrity="sha512-QMUqEPmhXq1f3DnAVdXvu40C8nbTgxvBGvNruP6RFacy3zWKbNTmx7rdQVVM2gkd2auCWhlPYtcW2tHwzso4SA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <script>
+            jQuery('form.mphb_sc_checkout-form').attr('autocomplete', 'off');
+            if (jQuery("#mphb_phone").length) {            
+                var input = document.querySelector("#mphb_phone");
+                var phoneField = window.intlTelInput(input, {
+                    onlyCountries: ["al", "ad", "at", "by", "be", "ba", "bg", "hr", "cz", "dk",
+                                    "ee", "fo", "fi", "fr", "de", "gi", "gr", "va", "hu", "is", "ie", "it", "lv",
+                                    "li", "lt", "lu", "mk", "mt", "md", "mc", "me", "nl", "no", "pl", "pt", "ro",
+                                    "ru", "sm", "rs", "sk", "si", "es", "se", "ch", "ua", "gb", "us"],
+                                    utilsScript: "../../build/js/utils.js?1613236686837",
+                        initialCountry: 'nl',
+                        autoPlaceholder: 'on',
+                        preferredCountries: ["nl"],
+                        separateDialCode: false,
+                        hiddenInput: "full_phone",
+                        utilsScript: 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/16.0.8/js/utils.js'
+                });
+                jQuery('form.mphb_sc_checkout-form').on('submit', function (e){
+                    if (iti.isValidNumber()) {
+                        jQuery("#mphb_phone").val(phoneField.getNumber(intlTelInputUtils.numberFormat.E164));
+                        return true; 
+                    } else {
+                        return false;
+                    }
+                });
+            }
+        </script>
+    <?php
+
+}
+add_action('wp_footer', 'phone_field');
+
